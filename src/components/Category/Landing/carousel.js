8ed4a047f89e1a7581c4fe9ba7d28d530c08a499
Nathalia <indigodreamer11@hotmail.com>
2019-08-19 15:24:08 -0500

    Merge profile&Home

--- src/components/Category/Landing/carousel.js
+++ src/components/Category/Landing/carousel.js
@@ -4,7 +4,7 @@ import './carousel.css';
 import {Textfield, Grid, Cell, Card, CardActions, CardTitle} from 'react-mdl'
 import Category from '../Category';
 
-/* Subir imágenes actualizadas (Relacionadas con CEO) a firebase y cambiar los links */
+
 
 const imgUrls = [
 	"https://firebasestorage.googleapis.com/v0/b/kundalini-agent.appspot.com/o/Screen%20Shot%202019-08-19%20at%2011.22.40%20AM.png?alt=media&token=b426f034-46e5-4889-b51a-0578fa432098","https://firebasestorage.googleapis.com/v0/b/kundalini-agent.appspot.com/o/BACK8.png?alt=media&token=49b317c6-fa54-4c23-9149-0a958959cb81","https://firebasestorage.googleapis.com/v0/b/kundalini-agent.appspot.com/o/BACK6.png?alt=media&token=84e31db9-2b76-4ec8-a44e-29312e53c750","https://firebasestorage.googleapis.com/v0/b/kundalini-agent.appspot.com/o/back7.png?alt=media&token=483bd398-43c7-4836-8914-1aae28be0042","https://firebasestorage.googleapis.com/v0/b/kundalini-agent.appspot.com/o/BACK9.png?alt=media&token=587f29b8-f1f2-446f-a594-3da17fca8cbe"
@@ -51,6 +51,8 @@ class Carousel extends Component {
 				<ImageSlide url={ imgUrls[this.state.currentImageIndex] } />
 				<Arrow direction="right" clickFunction={ this.nextSlide } glyph="&#9654;" />
 
+				{/* Categories--> Routing to take each of them to a new component */}
+
 				<div class="card bg-dark text-white text-center mb-3">
   					<div class="card-body">
   					  <h3 class="card-title">Graphics and design</h3>
@@ -73,43 +75,43 @@ class Carousel extends Component {
 
 				<div class="card-columns">
 
-	<div class="card bg-light p-3 text-center">
-    <blockquote class="card-body">
-      <p> Payment is released to the freelancer once you’re pleased and approve the work you get.</p>
-      <footer>
-        <small>
-		Payment Protection, Guaranteed
-        </small>
-      </footer>
-    </blockquote>
-  </div>
+					{/* Advantages and general information */}
 
-  <div class="card bg-warning text-white p-3 text-center">
-    <blockquote class="card-body">
-      <p>Find any service within minutes and know exactly what you’ll pay. No hourly rates, just a fixed price.</p>
-      <footer>
-        <small>
-		Know The Price Upfront
-        </small>
-      </footer>
-    </blockquote>
-  </div>
+ <				div class="card bg-light p-3 text-center">
+  				  <blockquote class="card-body">
+  				    <p> Payment is released to the freelancer once you’re pleased and approve the work you get.</p>
+  				    <footer>
+  				      <small>
+						Payment Protection, Guaranteed
+  				      </small>
+  				    </footer>
+  				  </blockquote>
+  				</div>
+
+  				<div class="card bg-warning text-white p-3 text-center">
+  				  <blockquote class="card-body">
+  				    <p>Find any service within minutes and know exactly what you’ll pay. No hourly rates, just a fixed price.</p>
+  				    <footer>
+  				      <small>
+							Know The Price Upfront
+  				      </small>
+  				    </footer>
+  				  </blockquote>
+  				</div>
 
 
-  <div class="card bg-light p-3 text-center">
-    <blockquote class="card-body">
-      <p> Κοῖος is here for you, anything from answering any questions to resolving any issues, at any time.</p>
-      <footer>
-        <small>
-		We’re Here For You 24/7
-        </small>
-      </footer>
-    </blockquote>
-  </div>
+  				<div class="card bg-light p-3 text-center">
+  				  <blockquote class="card-body">
+  				    <p> Κοῖος is here for you, anything from answering any questions to resolving any issues, at any time.</p>
+  				    <footer>
+  				      <small>
+						We’re Here For You 24/7
+  				      </small>
+  				    </footer>
+  				  </blockquote>
+  				</div>
+				</div>
 
-</div>
-
-			
 			</div>
 		);
 	}
